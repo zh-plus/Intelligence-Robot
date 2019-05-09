24a9b03a976c974c19c3861bc2f3af67e75f43ff
zhenghao <zhenghaosustc@gmail.com>
2019-05-09 23:54:41 +0800

    HW8 done

---
+++ HW8/HW8.md
@@ -0,0 +1,33 @@
+### HW7
+
+##### Q1
+
+**Warning: the python code is base on sympy, please install this package to run the source code!**
+
+Set [x, y] = (150, 120), a = 200, [x0, y0] = (0, 0) as parameters.
+
+The r is added ($mu = 0, theta = 1​$) gaussian noise.
+
+Only use three of r to solve the equation to avoid over-deterministic, which would cause unsolvable equations.
+
+![q1](E:\Workspace\Matlab\Robotics\HW8\q1.png)
+
+
+
+##### Q2
+
+Following the guide: 
+
+![q2](E:\Workspace\Matlab\Robotics\HW8\q2_1.png)
+
+![q3](E:\Workspace\Matlab\Robotics\HW8\q2_2.png)
+
+
+
+##### Q3
+
+Following the guide: 
+
+![q3_1](E:\Workspace\Matlab\Robotics\HW8\q3_1.png)
+
+![q3_2](E:\Workspace\Matlab\Robotics\HW8\q3_2.png)
--- HW8/q1.py
+++ HW8/q1.py
@@ -2,11 +2,10 @@ from sympy import Symbol, solve, nsolve, symbols, Eq
 from math import sqrt
 from functools import partial
 
+import matplotlib.pyplot as plt
+
 import random
 
-
-# x = Symbol('x')
-# print(solve(x * 3 - 6, x))
 
 def get_distance(p1, p2):
     return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
@@ -16,12 +15,12 @@ def add_gaussian_noise(x):
     return x + random.gauss(0, 1)
 
 
-if __name__ == '__main__':
-    x, y = 150, 120
-    x0, y0 = 0, 0  # Unknown
-    a = 200  # Unknown
+def get_points(a, x0, y0):
+    return [(x0, y0), (x0 + a, y0), (x0, y0 + a), (x0 + a, y0 + a)]
 
-    points = [(x0, y0), (x0 + a, y0), (x0, y0 + a), (x0 + a, y0 + a)]
+
+def do_hw(x, y, x0_un, y0_un, a_un):
+    points = get_points(a_un, x0_un, y0_un)
 
     xy_distance = partial(get_distance, (x, y))
     r = [xy_distance(points[0]), xy_distance(points[1]), xy_distance(points[2]), xy_distance(points[3])]
@@ -29,16 +28,48 @@ if __name__ == '__main__':
     print(r)
 
     a, x0, y0 = symbols('a x0 y0')
-    points = [(x0, y0), (x0 + a, y0), (x0, y0 + a), (x0 + a, y0 + a)]
+    points = get_points(a, x0, y0)
 
     functions = [
         Eq(xy_distance(points[0]), r[0]),
         Eq(xy_distance(points[1]), r[1]),
-        Eq(xy_distance(points[2]) + xy_distance(points[3]), r[2] + r[3]),
-        # Eq(xy_distance(points[3]), r[3])
+        Eq(xy_distance(points[2]), r[2]),
+        # Eq(xy_distance(points[3]), r[3])  # 3 equations is enough to solve this
     ]
 
     result = solve(functions, [a, x0, y0])
     print(result)
 
+    # the nearest one is the solution
+    solution = min(result, key=lambda x: sqrt((a_un - x[0]) ** 2 + (x0_un - x[1]) ** 2 + (y0_un - x[2]) ** 2))
+    print(solution)
 
+    solved_points = get_points(*solution)
+
+    return solved_points
+
+
+def draw(x, y, points):
+    plt.scatter(x, y)
+
+    X, Y = zip(*points)
+    plt.scatter(X, Y)
+
+    # add annotations
+    plt.annotate('[x, y] = ({:.2f}, {:.2f})'.format(x, y), xy=(x, y), xytext=(x - 40, y + 8))
+
+    for p in points:
+        plt.annotate('({:.2f}, {:.2f})'.format(*p), xy=p, xytext=(p[0] + 5, p[1] + 3))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Set parameters
+    x, y = 150, 120
+    x0_un, y0_un = 0, 0  # Unknown
+    a_un = 200  # Unknown
+
+    solved_points = do_hw(x, y, x0_un, y0_un, a_un)
+
+    draw(x, y, solved_points)
